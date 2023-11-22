9c419a2d0a1746a44faa055550f83cb9ab49e8d4
Boniface <Cheruiyotbony@gmail.com>
2023-11-22 19:08:09 +0300

    commited

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,19 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{
+        projects.map((project)=>{
+      return(
+        <div key={project.id}>
+          <p>{project.name}</p>
+          <p>{project.about}</p>
+          <p>{project.technology}</p>
+
+        </div>
+      )
+
+        })
+      }</div>
     </div>
   );
 }
